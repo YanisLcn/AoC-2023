32444057edb3e7a40c42d46ed4a8b1e945639b97
Yago Iglesias <yago.iglesias.vazquez@gmail.com>
2023-12-01 10:54:23 +0100

    feat!: Simplify API
    In order to simplify the API, the following changes have been made:
    - Day is no longer an optional argument but a flag
    - Part is now the only optional argument
    
    BREAKING CHANGE: Day is no longer an optional argument but a flag

--- src/parser.rs
+++ src/parser.rs
@@ -2,8 +2,11 @@ use clap_derive::Parser;
 
 #[derive(Parser)]
 pub struct CommandArgument {
+    #[clap(short, long)]
     pub day: Option<u32>,
+
     pub part: Option<u32>,
+
     #[clap(short, long)]
     pub publish: bool,
 }
