bbcc7c4dd87a8ff3080a0583dac6aa12823b422d
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-12-02 20:16:31 +0100

    feat(day2): Part 1

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,7 +1,48 @@
+use itertools::Itertools;
+
+const MAX_COLORS: [(u32, &'static str); 3] = [(12, "red"), (13, "green"), (14, "blue")];
+
+fn get_id(game: &str) -> u32 {
+    game.split(" ")
+        .skip(1)
+        .collect::<Vec<&str>>()
+        .pop()
+        .unwrap()
+        .parse::<u32>()
+        .unwrap()
+}
+
+fn extract_colors(colors: &str) -> Vec<Vec<(u32, &str)>> {
+    colors
+        .split("; ")
+        .map(|part: &str| {
+            part.split(", ")
+                .map(|clr: &str| clr.split(" ").collect_tuple::<(&str, &str)>().unwrap())
+                .map(|(nb, clr): (&str, &str)| (nb.parse::<u32>().unwrap(), clr))
+                .collect::<Vec<(u32, &str)>>()
+        })
+        .collect::<Vec<Vec<(u32, &str)>>>()
+}
+
 pub fn part1() -> String {
     let path = "input/day2.txt";
     let input = std::fs::read_to_string(path).unwrap();
-    "".to_string()
+    format!(
+        "{}",
+        input
+            .lines()
+            .map(|l| l.split(": ").collect_tuple::<(&str, &str)>().unwrap())
+            .map(|(id, clrs)| (get_id(id), extract_colors(clrs)))
+            .filter(|(_, clrs)| {
+                clrs.iter().all(|part| {
+                    part.iter().all(|(nb, clr)| {
+                        MAX_COLORS.iter().find(|(max_nb, c)| clr == c).unwrap().0 >= *nb
+                    })
+                })
+            })
+            .map(|(id, _)| id)
+            .sum::<u32>()
+    )
 }
 
 pub fn part2() -> String {
