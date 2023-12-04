24f09f3fb51a66082c806200943fe633cf8cdfff
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-12-04 22:51:03 +0100

    feat(day4): Part 2

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 use itertools::Itertools;
 
 pub fn part1() -> String {
@@ -42,5 +44,47 @@ pub fn part1() -> String {
 pub fn part2() -> String {
     let path = "input/day4.txt";
     let input = std::fs::read_to_string(path).unwrap();
-    "".to_string()
+    let scratchpads: &mut Vec<u32> = &mut Vec::new();
+    input
+        .lines()
+        .map(|l| l.split(": ").collect_tuple::<(&str, &str)>().unwrap())
+        .map(|(_, numbers)| {
+            numbers
+                .split(" | ")
+                .collect_tuple::<(&str, &str)>()
+                .unwrap()
+        })
+        .map(|(winners, values)| {
+            (
+                winners
+                    .split(" ")
+                    .filter_map(|v| v.parse::<u32>().ok())
+                    .collect::<Vec<u32>>(),
+                values
+                    .split(" ")
+                    .filter_map(|v| v.parse::<u32>().ok())
+                    .collect::<Vec<u32>>(),
+            )
+        })
+        .map(|(w, v)| -> u32 {
+            v.iter()
+                .fold(0_u32, |t, val| if w.contains(val) { t + 1 } else { t })
+        })
+        .fold(0, |total_pads, total_winners| -> u32 {
+            let actual = if scratchpads.len() == 0 {
+                1
+            } else {
+                1 + scratchpads.remove(0)
+            };
+            let len: u32 = scratchpads.len().try_into().unwrap();
+            for index in 0..total_winners {
+                if index < len {
+                    scratchpads[index as usize] += actual;
+                } else {
+                    scratchpads.push(actual);
+                }
+            }
+            total_pads + actual
+        })
+        .to_string()
 }
